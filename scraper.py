b2ee3bc9fd9eeb5869fcee9cef269163e1cfad11
Alex <156462384+ailothh@users.noreply.github.com>
2025-01-21 11:45:57 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import requests
 import time
 import re
 from bs4 import BeautifulSoup
-
+# watermelon_elf Discord any errors
 # Initialize variables
 test = False
 useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
